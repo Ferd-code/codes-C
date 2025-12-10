e67e4d4ccf9265bdb466f282667598f7e8a0f51f
Ferd-code <ferdaouselkhayati695@gmail.com>
2025-12-10 05:49:26 +0100

    First commit

--- code1.c
+++ code1.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 int main()
 {
-    printf("comment vas tu ");
+    printf(" Hello world!\n");
+    int x;
+    x=3;
+    printf("x=%d\n",x);
+    float a,b;
+    a=5.5;
+    b=6.9;
+    printf("A=%.2f\n,B=%.2f\n",a,b);
+
     return 0;
 }
